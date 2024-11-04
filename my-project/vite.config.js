64b7ad8c86eaadd95a544461e52b764530f1eaeb
YOUNESyoyou123 <djjdjfxdjdid@gmail.com>
2024-11-04 17:33:47 +0100

    dd

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/",
+  base: "/youy/", // Make sure this matches your repository name
 });
